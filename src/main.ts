08d7277d24bfdb4251af48eddcc74b54ee9caa1e
hyeonwoo <hyeonwoo.v24@gmail.com>
2025-08-20 13:34:44 +0900

    cors update

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,8 @@ async function bootstrap() {
   app.enableCors({
     origin:
       process.env.NODE_ENV === 'production'
-        ? ['https://adm.hi.hyeonwoo.com', 'http://localhost:8081']
-        : ['http://localhost:8081'],
+        ? ['https://adm.hi.hyeonwoo.com', 'https://hi.hyeonwoo.com']
+        : ['http://localhost:8081', 'http://localhost:8082'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
